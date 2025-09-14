2b85302f1c4085251ccd12eeee1b2f811d59a703
Om Prakash Upadhyay <omprakashupadhyay886@gmail.com>
2025-09-14 12:18:28 +0530

    Real Time Code Editor

---
+++ backend/index.js
@@ -0,0 +1,47 @@
+import { Socket } from 'dgram';
+import express from 'express'
+import http from 'http'
+import { Server } from 'socket.io';
+
+
+const app = express()
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+    },
+})
+
+const rooms = new Map();
+
+io.on("connection", (socket)=>{
+    console.log("User connected", socket.id);
+
+    let currentRoom = null;
+    let currentUser = null;
+
+    socket.on("john",({roomId, userName})=>{
+        if(currentRoom){
+            socket.leave(currentRoom)
+            rooms.get(currentRoom).delete(currentUser)
+            io.to(currentRoom).emit("userJoined", Array.from(rooms.get(currentRoom)))
+        }
+
+        currentRoom = roomId;
+        currentUser = userName;
+
+        socket.join(roomId)
+
+        if(!rooms.has(roomId)){
+            rooms.set(roomId,new Set());
+        }
+    })
+})
+
+const port = process.env.PORT || 5000;
+
+server.listen(port,()=>{
+    console.log('server is working properly on port 5000')
+})
